ec8783724a9b705e1c088dbff64a37a75a56bfff
WonjinCODE <92743331+WonjinCODE@users.noreply.github.com>
2021-11-05 18:48:29 +0900

    Create Card.cs

---
+++ Card.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer card;
+    [SerializeField] SpriteRenderer character;
+    [SerializeField] TMP_Text nameTMP;
+    [SerializeField] TMP_Text attackTMP;
+    [SerializeField] TMP_Text healthTMP;
+    [SerializeField] Sprite cardFront;
+    [SerializeField] Sprite cardBack;
+
+    public Item item;
+    bool isFront;
+
+    public void Setup(Item item, bool isFront)
+    {
+        this.item = item;
+        this.isFront = isFront;
+
+        if (this.isFront)
+        {
+            character.sprite = this.item.sprite;
+            nameTMP.text = this.item.name;
+            attackTMP.text = this.item.attack.ToString();
+            healthTMP.text = this.item.health.ToString();
+        }
+        else
+        {
+            card.sprite = cardBack;
+            nameTMP.text = "";
+            attackTMP.text = "";
+            healthTMP.text = "";
+        }
+    }
+}
